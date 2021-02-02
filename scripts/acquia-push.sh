ef551353d1ede199037337f0adc22455cda1f83e
Matthew Tift <matthew.tift@lullabot.com>
2021-02-02 09:11:35 -0600

    Update script

--- scripts/acquia-push.sh
+++ scripts/acquia-push.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-# Get the lastest code into master-compiled
+# Configure Git repository.
+GIT_URL="git@github.com:mtift/deploy-example.git"
+GIT_REMOTE_NAME="acquia"
+
+# Directories or path patterns to force commit.
+FORCE_COMMIT_PATTERNS="vendor \
+/web/core \
+/web/modules/contrib \
+/web/themes/contrib \
+/web/profiles/contrib \
+/web/libraries"
+
+# Add a warning message. You are about to update the following directories
+# in the following Git repository.
+
+# Make sure the repo exists as a remote.
+if git remote|grep ${GIT_REMOTE_NAME}; then
+  git remote set-url ${GIT_REMOTE_NAME} ${GIT_URL}
+else
+  git remote add ${GIT_REMOTE_NAME} ${GIT_URL}
+fi
+
+# Get the lastest code into master-compiled.
 git checkout master
-git pull --no-edit
+# Accept the default commit message.
+git pull ${GIT_REMOTE_NAME} master --no-edit
 git checkout master-compiled
 git fetch
-git merge origin/master
+git merge ${GIT_REMOTE_NAME}/master
 
+# Don't ignore the composer-generated files.
 mv -f .gitignore.acquia .gitignore
 
 # Build the theme assets
@@ -16,14 +40,6 @@ mv -f .gitignore.acquia .gitignore
 
 # Install files with composer
 composer install --optimize-autoloader
-
-# These are the directories or path patterns to force commit.
-FORCE_COMMIT_PATTERNS="vendor \
-/web/core \
-/web/modules/contrib \
-/web/themes/contrib \
-/web/profiles/contrib \
-/web/libraries"
 
 # Clean out all .git dirs from any directories.
 for PATTERN in $FORCE_COMMIT_PATTERNS; do
@@ -55,5 +71,5 @@ git push --follow-tags
 git checkout --quiet master
 
 # Replace the ignored files
-git checkout origin/master -- .gitignore.acquia
+git checkout ${GIT_REMOTE_NAME}/master -- .gitignore.acquia
 git checkout .gitignore
