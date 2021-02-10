4b020729e5730ba81198e7977188d0695aec8d71
Matthew Tift <matthew.tift@lullabot.com>
2021-02-10 13:49:00 -0600

    Remove the honeypot module

---
+++ scripts/deploy-script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#############
+# CONFIGURE #
+#############
+
+# Make sure the js and css directories exist
+mkdir -p web/themes/custom/deploy_example_theme/js
+mkdir -p web/themes/custom/deploy_example_theme/css
+
+# Directories or path patterns to force commit.
+FORCE_COMMIT_PATTERNS="vendor \
+web/core \
+web/modules/contrib \
+web/themes/contrib \
+web/themes/custom/deploy_example_theme/css \
+web/themes/custom/deploy_example_theme/js \
+web/themes/custom/deploy_example_theme/node_modules \
+web/profiles/contrib \
+web/libraries"
+
+# Clean out all .git dirs from any directories.
+for PATTERN in $FORCE_COMMIT_PATTERNS; do
+  echo $PATTERN
+  if [ -d $PATTERN ]; then
+    find $PATTERN -type d -name .git | xargs rm -rf
+    git add --force $PATTERN &> /dev/null
+  fi
+done
+
+git status
+
+echo "\nComplete! The master-compiled branch is ready for verification"
