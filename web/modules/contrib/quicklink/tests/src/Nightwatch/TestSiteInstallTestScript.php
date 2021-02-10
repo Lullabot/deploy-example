61f0efc41345f2ff1af83990507c0cd7c36b2cf4
Matthew Tift <matthew.tift@lullabot.com>
2021-02-10 16:02:47 -0600

    Whatever

---
+++ web/modules/contrib/quicklink/tests/src/Nightwatch/TestSiteInstallTestScript.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\TestSite;
+
+/**
+ * Setup file used by TestSiteApplicationTest.
+ *
+ * @see \Drupal\Tests\Scripts\TestSiteApplicationTest
+ */
+class TestSiteInstallTestScript implements TestSetupInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setup() {
+    \Drupal::service('module_installer')->install(['quicklink']);
+    $quicklink_settings = \Drupal::configFactory()->getEditable('quicklink.settings');
+    $quicklink_settings->set('enable_debug_mode', TRUE);
+    $quicklink_settings->save();
+  }
+
+}
